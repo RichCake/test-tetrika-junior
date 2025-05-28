1be58a5f0d1633d23232b1ad1af545f575d24f06
Arsenii Karpov <abs2016123@gmail.com>
2025-05-28 22:48:01 +0300

    task 1

---
+++ task1/solution.py
@@ -0,0 +1,35 @@
+from functools import wraps
+from inspect import get_annotations
+
+
+def strict(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        var_annotations = get_annotations(func)
+        return_type = var_annotations.pop("return", None)
+        for i, (var, var_type) in enumerate(var_annotations.items()):
+            # Позиционные аргументы всегда идут первыми как в списке args, так и в сигнатуре.
+            # Также учитывается что длина сигнатуры всегда равна длине args + kwargs
+            if i < len(args):
+                check_val = args[i]
+            else:
+                check_val = kwargs[var]
+
+            if not isinstance(check_val, var_type):
+                raise TypeError(f"Argument '{var}' expected type {var_type.__name__}, got {type(check_val).__name__}")
+
+        out = func(*args, **kwargs)
+        if return_type and not isinstance(out, return_type):
+            raise TypeError(f"Return value expected type {return_type.__name__}, got {type(out).__name__}")
+        return out
+
+    return wrapper
+
+
+@strict
+def sum_two(a: int, b: int) -> int:
+    return a + b
+
+
+print(sum_two(b=1, a=2))  # >>> 3
+print(sum_two(1, 2.4))  # >>> TypeError
